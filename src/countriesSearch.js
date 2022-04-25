940b46f3c59176e1f67e2ed845fd334a81b57017
RJHARENBERG <reinardharenberg@gmail.com>
2022-04-25 09:46:06 +0200

    fetchPeculiarCountriesData werkt

--- src/countriesSearch.js
+++ src/countriesSearch.js
@@ -1,1 +1,23 @@
-console.log("test")
+import axios from "axios";
+
+
+async function fetchPeculiarCountriesData() {
+    let countriesData;
+    try {
+        const result = await axios.get("https://restcountries.com/v2/name/peru");
+        countriesData = result.data.map((data) => {
+            return data;
+        })
+    } catch (e) {
+        console.error(e);
+    }
+    return countriesData;
+}
+
+function testData (){
+    fetchPeculiarCountriesData().then(data => {
+        console.log(data[0])
+    });
+}
+
+testData();
